0c38cdc965120ae848c20d81bfbd4db3d6a48b42
karthikeyan <karthikeyan@localhost>
2022-01-02 22:02:41 +0530

    updated the logic to package the application as a WAR

--- src/main/java/com/businesslisting/kubrii/main/KubriiApiApplication.java
+++ src/main/java/com/businesslisting/kubrii/main/KubriiApiApplication.java
@@ -3,6 +3,7 @@ package com.businesslisting.kubrii.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = "com.businesslisting.kubrii" )
 @EntityScan("com.businesslisting.kubrii.entity")
 @EnableJpaRepositories("com.businesslisting.kubrii.repository")
-public class KubriiApiApplication {
+public class KubriiApiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(KubriiApiApplication.class, args);
